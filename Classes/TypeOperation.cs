a4e3656c10f037c655003c44f71a2dde48820f81
gsavichev <galesavich@gmail.com>
2024-01-18 09:13:30 +0500

    Реализованные классы. Савичев

---
+++ Classes/TypeOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savichev18pr.Classes
+{
+    public class TypeOperation
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public TypeOperation(int id, string name, string description)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+
+        public static List<TypeOperation> AllTypeOperation()
+        {
+            List<TypeOperation> allTypeOperation = new List<TypeOperation>();
+            allTypeOperation.Add(new TypeOperation(1, "Печать", ""));
+            allTypeOperation.Add(new TypeOperation(2, "Копия", ""));
+            allTypeOperation.Add(new TypeOperation(3, "Сканирование", ""));
+            allTypeOperation.Add(new TypeOperation(4, "Ризограф", ""));
+            return allTypeOperation;
+        }
+    }
+}
